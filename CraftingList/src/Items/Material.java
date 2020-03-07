59169dc3b574a1d5ccd51c6f7c68cc38d6e586da
PC <PC@DESKTOP-J04BAB0>
2020-03-06 21:59:13 -0500

    Started project

--- CraftingList/src/Items/Material.java
+++ CraftingList/src/Items/Material.java
@@ -1,5 +1,14 @@
 package Items;
 
 public class Material {
+	String name;
+	int level;
+	String gatherer;
+	
+	public Material(String name, int level, String gatherer){
+		this.name = name;
+		this.level = level;
+		this.gatherer = gatherer;
+	}
 
 }
