a9b0fd6c264faed65765c8bf61fb88cb07728930
Katelyn Bunker <kerbunker@gmail.com>
2020-03-26 15:04:10 -0400

    Added ArrayList of materials and a userInterface until GUI Implemented

--- CraftingList/src/Items/Material.java
+++ CraftingList/src/Items/Material.java
@@ -1,11 +1,12 @@
-package Items;
+package craftingList;
 
 public class Material {
 	String name;
 	int level;
 	String gatherer;
 	
-	public Material(String name, int level, String gatherer){
+	public Material( String name, int level, String gatherer )
+	{
 		this.name = name;
 		this.level = level;
 		this.gatherer = gatherer;
@@ -14,13 +15,17 @@ public class Material {
 	public String getName() {
 		return name;
 	}
-	
 	public int getLevel() {
 		return level;
 	}
-	
 	public String getGatherer() {
 		return gatherer;
+	}
+	
+	public void print() {
+		System.out.println("Name: " + name);
+		System.out.println("Level: "+ level);
+		System.out.println("Gatherer: " + gatherer);
 	}
 
 }
